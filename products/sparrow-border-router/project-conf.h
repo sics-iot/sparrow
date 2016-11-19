5931251724bf402870f0dade2c66cc1558855726
Ang, Chee Hong <chee.hong.ang@intel.com>
2016-11-19 15:46:46 +0800

    Add MAKE_WITH_LLSEC_LEVEL for iot-u10/remote/sparrow-border-router to enable link layer security with 1 to 7 level
    Signed-off-by: Ang, Chee Hong <chee.hong.ang@intel.com>

--- products/sparrow-border-router/project-conf.h
+++ products/sparrow-border-router/project-conf.h
@@ -143,4 +143,20 @@
 
 #define LATENCY_STATISTICS 1
 
+#if LLSEC_CONF_LEVEL
+#undef LLSEC802154_CONF_ENABLED
+#define LLSEC802154_CONF_ENABLED          1
+#undef NETSTACK_CONF_FRAMER
+#define NETSTACK_CONF_FRAMER              noncoresec_framer
+#undef NETSTACK_CONF_LLSEC
+#define NETSTACK_CONF_LLSEC               noncoresec_driver
+#undef NONCORESEC_CONF_SEC_LVL
+#define NONCORESEC_CONF_SEC_LVL           LLSEC_CONF_LEVEL
+
+#define NONCORESEC_CONF_KEY { 0x00 , 0x01 , 0x02 , 0x03 , \
+                              0x04 , 0x05 , 0x06 , 0x07 , \
+                              0x08 , 0x09 , 0x0A , 0x0B , \
+                              0x0C , 0x0D , 0x0E , 0x0F }
+#endif /* LLSEC_CONF_LEVEL */
+
 #endif /* PROJECT_ROUTER_CONF_H_ */
